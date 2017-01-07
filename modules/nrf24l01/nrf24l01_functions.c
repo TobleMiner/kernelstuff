1729ee3bf7eacf2d62e1d28332a1237505ffd698
Tobias Schramm <tobleminer@gmail.com>
2017-01-07 00:20:15 +0000

    Add address width getter

--- modules/nrf24l01/nrf24l01_functions.c
+++ modules/nrf24l01/nrf24l01_functions.c
@@ -2,6 +2,7 @@
 
 #include "nrf24l01_core.h"
 #include "nrf24l01_reg.h"
+#include "partregmap.h"
 
 int nrf24l01_set_channel(struct nrf24l01_t* nrf, unsigned int ch)
 {
@@ -18,3 +19,7 @@ int nrf24l01_get_channel(struct nrf24l01_t* nrf, unsigned int* ch)
 	return regmap_write_bits(nrf->regmap_short, NRF24L01_REG_RF_SETUP, NRF24L01_MASK_TXPWR);
 }*/
 
+int nrf24l01_get_address_width(struct nrf24l01_t* nrf, unsigned int* width)
+{
+	return partreg_table_read(nrf->reg_table, NRF24L01_VREG_SETUP_AW_AW, width, 1);
+}
