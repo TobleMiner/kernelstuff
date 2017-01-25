c880f67324f39cd53805625f4cd31056525a8bf1
Tobias Schramm <tobleminer@gmail.com>
2017-01-25 23:50:28 +0000

    Add mode output on init

--- modules/nrf24l01/nrf24l01_core.c
+++ modules/nrf24l01/nrf24l01_core.c
@@ -126,6 +126,7 @@ static int nrf24l01_probe(struct spi_device* spi)
 	{
 		nrf24l01_dev->mode_flags = be32_to_cpup(of_nrf_mode);
 	}
+	dev_info(&spi->dev, "nrf mode: %u\n", nrf24l01_dev->mode_flags);
 	init_waitqueue_head(&nrf24l01_dev->rx_queue);
 	init_waitqueue_head(&nrf24l01_dev->tx_queue);
 	if((err = nrf24l01_create_worker(nrf24l01_dev)))
