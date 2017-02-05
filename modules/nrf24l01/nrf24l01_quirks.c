a21b7742847400a4fc0cf31121b00d97ee343e8f
Tobias Schramm <tobleminer@gmail.com>
2017-02-05 13:23:28 +0100

    Clean up nrf tx_fifo quirk test

--- modules/nrf24l01/nrf24l01_quirks.c
+++ modules/nrf24l01/nrf24l01_quirks.c
@@ -7,13 +7,12 @@ int nrf24l01_test_unflushable_fifo(struct nrf24l01_t* nrf)
 {
 	int err;
 	unsigned int val;
-	unsigned char* data[32];
-	if((err = nrf24l01_spi_write_tx_pld(nrf, data, 32)))
-		return err;
-	if((err = nrf24l01_spi_write_tx_pld(nrf, data, 32)))
-		return err;
-	if((err = nrf24l01_spi_write_tx_pld(nrf, data, 32)))
-		return err;
+	unsigned char data[32];
+	for(val = 0; val < 3; val++)
+	{
+		if((err = nrf24l01_spi_write_tx_pld(nrf, data, 32)))
+			return err;
+	}
 	if((err = nrf24l01_flush_tx(nrf)))
 		return err;
 	if((err = nrf24l01_get_status_tx_full(nrf, &val)))
