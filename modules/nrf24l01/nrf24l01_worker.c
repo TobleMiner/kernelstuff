090b7823b1e81cfb4a228be9a1abe9fbb9bb8bc7
Tobias Schramm <tobleminer@gmail.com>
2017-05-20 17:48:17 +0200

    Add some comments on the worker thread Hopefully this will keep people from smashing their head into the wall too much

--- modules/nrf24l01/nrf24l01_worker.c
+++ modules/nrf24l01/nrf24l01_worker.c
@@ -19,6 +19,7 @@ static int nrf24l01_worker_do_work(void* ctx)
 	struct nrf24l01_t* nrf = (nrf24l01_t*) ctx;
 	while(!kthread_should_stop())
 	{
+		// Wait for IRQ
 		if((err = down_timeout(&nrf->worker.sema, HZ)))
 		{
 			if(err != -ETIME)
@@ -26,45 +27,60 @@ static int nrf24l01_worker_do_work(void* ctx)
 				dev_err(&nrf->spi->dev, "Worker thread failed! err=%d\n", err);
 //				do_exit(err);
 			}
+			// Check for exit condition once per IRQ/second
 			if(kthread_should_stop())
 				break;
 			continue;
 		}
 		dev_dbg(&nrf->spi->dev, "Event!\n");
+
+		// Check rx data ready flag
 		if((err = nrf24l01_get_status_rx_dr(nrf, &data)))
 		{
 			dev_err(&nrf->spi->dev, "Failed to get rx_dr flag: %d\n", err);
 		}
 		if(data)
 		{
+			// Wake up rx queue if data has been received
 			wake_up_interruptible(&nrf->rx_queue);
+			// Clear rx data ready flag
 			if((err = nrf24l01_set_status_rx_dr(nrf, 1)))
 			{
 				dev_err(&nrf->spi->dev, "Failed to clear rx_dr flag: %d\n", err);
 			}
 		}
+
+		// Check tx data sent flag
 		if((err = nrf24l01_get_status_tx_ds(nrf, &data)))
 		{
 			dev_err(&nrf->spi->dev, "Failed to get tx_ds flag\n");
 		}
 		if(data)
 		{
+			// Wake up tx queue if space in tx fifo became available
 			wake_up_interruptible(&nrf->tx_queue);
+			// Clear tx data sent flag
 			if((err = nrf24l01_set_status_tx_ds(nrf, 1)))
 			{
 				dev_err(&nrf->spi->dev, "Failed to clear tx_ds flag: %d\n", err);
 			}
+			// Make sure no tx opertaion is in progress
 			mutex_lock(&nrf->m_tx_path);
+			// Check for empty tx queue
 			if((err = nrf24l01_get_fifo_tx_empty(nrf, &data)))
 			{
 				dev_err(&nrf->spi->dev, "Failed to check fifo status\n");
 				goto tx_fifo_empty_mutex;
 			}
+			// Switch back to rx/powersave if tx queue is empty
 			if(data)
 			{
+				// Check if we have power saving enabled
 				if(nrf24l01_get_mode_low_pwr(nrf))
 				{
+					// Make sure nothing is screwing with the reader count
 					mutex_lock(&nrf->m_rx_path);
+					// Switch to rx if there are readers
 					if(nrf->num_readers > 0)
 					{
 						if((err = nrf24l01_set_rx(nrf)))
@@ -72,6 +88,7 @@ static int nrf24l01_worker_do_work(void* ctx)
 							dev_err(&nrf->spi->dev, "Failed to set nrf to RX: %d\n", err);
 						}
 					}
+					// Go to power down mode 
 					else
 					{
 						if((err = nrf24l01_shutdown(nrf)))
@@ -81,6 +98,7 @@ static int nrf24l01_worker_do_work(void* ctx)
 					}
 					mutex_unlock(&nrf->m_rx_path);
 				}
+				// Go back to rx
 				else
 				{
 					if((err = nrf24l01_set_rx(nrf)))
@@ -92,13 +110,18 @@ static int nrf24l01_worker_do_work(void* ctx)
 tx_fifo_empty_mutex:
 			mutex_unlock(&nrf->m_tx_path);
 		}
+
+		// Check for maximum retransmissions reached flag
 		if((err = nrf24l01_get_status_max_rt(nrf, &data)))
 		{
 			dev_err(&nrf->spi->dev, "Failed to get max_rt flag\n");
 		}
+		// Clear fifo if there were too many retransmissions
 		if(data)
 		{
 			dev_warn(&nrf->spi->dev, "Maximum number of retransmissions reached. Dropping tx fifo\n");
+
+			// Clear maxiumum retransmissions reached flag
 			if((err = nrf24l01_set_status_max_rt(nrf, 1)))
 			{
 				dev_err(&nrf->spi->dev, "Failed to reset max_rt flag\n");
@@ -110,6 +133,7 @@ tx_fifo_empty_mutex:
 				dev_err(&nrf->spi->dev, "Failed to flush tx fifo");
 			}
 			mutex_lock(&nrf->m_tx_path);
+			// Do more of the same powersaving stuff
 			if(nrf24l01_get_mode_low_pwr(nrf))
 			{
 				mutex_lock(&nrf->m_rx_path);
@@ -137,6 +161,7 @@ tx_fifo_empty_mutex:
 				}
 			}
 			mutex_unlock(&nrf->m_tx_path);
+			// Now there is space in the tx fifo for sure. Wake up writers
 			wake_up_interruptible(&nrf->tx_queue);
 		}
 	}
