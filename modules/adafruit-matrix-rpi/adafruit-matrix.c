a843a1726910665cef69b85b0c789832bb263d5a
Tobias Schramm <tobleminer@gmail.com>
2017-10-14 00:28:00 +0200

    Hack in gamma correction
    gamma = 2.8

--- modules/adafruit-matrix-rpi/adafruit-matrix.c
+++ modules/adafruit-matrix-rpi/adafruit-matrix.c
@@ -44,6 +44,24 @@ static uint32_t* adamtx_intermediate_frame;
 
 static unsigned long current_bcd_time = ADAMTX_BCD_TIME_NS;
 
+static uint8_t gamma_table[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
 #define ADAMTX_NUM_PANELS 2
 
 static struct matrix_ledpanel adamtx_matrix_up = {
@@ -130,7 +148,7 @@ void remap_frame(struct matrix_ledpanel** panels, char* from, int width_from, in
 			panel = matrix_get_panel_at_real(panels, ADAMTX_NUM_PANELS, j, i);
 			matrix_panel_get_position(&pos, panel, j, i);
 			offset = i * width_from * ADAMTX_PIX_LEN + j * ADAMTX_PIX_LEN;
-			to[pos.y * width_to + pos.x] = from[offset] | from[offset + 1] << 8 | from[offset + 2] << 16;
+			to[pos.y * width_to + pos.x] = gamma_table[(unsigned char)from[offset]] | gamma_table[(unsigned char)from[offset + 1]] << 8 | gamma_table[(unsigned char)from[offset + 2]] << 16;
 		}
 	}
 }
