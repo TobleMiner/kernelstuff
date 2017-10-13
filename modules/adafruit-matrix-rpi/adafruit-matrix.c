a91f2c048e20da5134e8ebe98d76efadfad2dd92
Tobias Schramm <tobleminer@gmail.com>
2017-10-13 23:29:46 +0200

    Apparently instruction reordering was not a good idea...

--- modules/adafruit-matrix-rpi/adafruit-matrix.c
+++ modules/adafruit-matrix-rpi/adafruit-matrix.c
@@ -182,9 +182,8 @@ void show_frame(struct adamtx_panel_io* frame, int bits, int rows, int columns)
 	{
 		for(j = 0; j < pwm_steps; j++)
 		{
-			getnstimeofday(&last);
-
 			ADAMTX_GPIO_HI(ADAMTX_GPIO_OE);
+			getnstimeofday(&last);
 			adamtx_clock_out_row(frame + i * pwm_steps * columns + j * columns, columns);
 			if(!j) {
 				adamtx_set_address(i);
